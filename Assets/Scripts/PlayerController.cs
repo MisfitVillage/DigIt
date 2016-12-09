5e65a76fe8af1001f8da3d73b3ec7b0eb704b0c8
Hugo Vieira <hugo.andre.vieira@gmail.com>
2016-12-09 17:11:49 +0000

    Added base scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	// Player Components
+	private Rigidbody _playerRb;
+
+
+	// Player Environment Variables
+	public float PlayerSpeed;
+
+	void Start()
+	{
+		_playerRb = gameObject.GetComponent<Rigidbody>();
+	}
+
+
+	void Update()
+	{
+	}
+
+
+	void OnCollisionEnter(Collision collision)
+	{
+	}
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+	// This function detects if the user swiped up on the screen and
+	// returns a boolean value accordingly
+	public static bool SwipedUp()
+	{
+		foreach (var touch in Input.touches)
+		{
+			if (touch.deltaPosition.sqrMagnitude != 0f)
+			{
+				// Get position delta for touch
+				Vector2 deltaPos = touch.deltaPosition;
+
+				// Calculate absolute values for x and y deltas.
+				// If y < x && y < 0f, means the user is swiping down, so return true.
+				if (Mathf.Abs(deltaPos.x) < Mathf.Abs(deltaPos.y))
+				{
+					return deltaPos.y > 0f;
+				}
+			}
+		}
+
+		return false;
+	}
+
+
+	// This function detects if the user swiped down on the screen and
+	// returns a boolean value accordingly
+	public static bool SwipedDown()
+	{
+		foreach (var touch in Input.touches)
+		{
+			if (touch.deltaPosition.sqrMagnitude != 0f)
+			{
+				// Get position delta for touch
+				Vector2 deltaPos = touch.deltaPosition;
+
+				// Calculate absolute values for x and y deltas.
+				// If y < x && y < 0f, means the user is swiping down, so return true.
+				if (Mathf.Abs(deltaPos.x) < Mathf.Abs(deltaPos.y))
+				{
+					return deltaPos.y < 0f;
+				}
+			}
+		}
+
+		return false;
+	}
+
+
+	// This function detects if the user swiped left on the screen and
+	// returns a boolean value accordingly
+	public static bool SwipedLeft()
+	{
+		foreach (var touch in Input.touches)
+		{
+			if (touch.deltaPosition.sqrMagnitude != 0f)
+			{
+				Vector2 deltaPos = touch.deltaPosition;
+				return deltaPos.x < 0f;
+			}
+		}
+
+		return false;
+	}
+
+
+	// This function detects if the user swiped right on the screen and
+	// returns a boolean value accordingly
+	public static bool SwipedRight()
+	{
+		foreach (var touch in Input.touches)
+		{
+			if (touch.deltaPosition.sqrMagnitude != 0f)
+			{
+				Vector2 deltaPos = touch.deltaPosition;
+				return deltaPos.x > 0f;
+			}
+		}
+
+		return false;
+	}
+}
